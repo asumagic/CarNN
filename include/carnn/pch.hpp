8d035ef0e7a3877cbbe136332e76257bf6c89456
Asu <sdelang@sdelang.fr>
2022-04-27 00:31:19 +0200

    Build improvements

---
+++ include/carnn/pch.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <tbb/tbb.h>
+#include <cereal/cereal.hpp>
+#include <cereal/archives/json.hpp>
+#include <box2d/box2d.h>
+#include <fmt/core.h>
+#include <spdlog/spdlog.h>
+#include <fstream>
